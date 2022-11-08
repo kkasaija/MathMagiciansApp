8cf521bc09be17aef57c1dc98ec3af947a005e0f
Kasaija Kenneth <kasaijak@gmail.com>
2022-11-08 18:18:05 +0300

    Add Quote component

---
+++ src/components/quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Quote.css';
+
+function Quotes() {
+  return (
+    <>
+      <main className="quotesParagraph">
+        <p className="paragraphQuote">
+          A man is like a fraction whose numerator is what he is
+          and whose denominator is what he thinks of himself.
+          The larger the denominator the smaller the fraction.
+          - Leo Tolstoy -
+        </p>
+      </main>
+    </>
+  );
+}
+export default Quotes;
